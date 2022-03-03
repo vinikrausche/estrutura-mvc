ec4e7bc3376efda6d30eff05281cd86112eff4e8
Vini <vinicius.krausche96@gmail.com>
2022-03-03 14:10:43 -0300

    Alteriei a classe HomeController.php

--- app/Controllers/Pages/HomeController.php
+++ app/Controllers/Pages/HomeController.php
@@ -4,15 +4,18 @@ namespace App\Controllers\Pages;
 
 use App\Utils\View;
 use App\Controllers\Pages\PageController;
+use App\Models\Entity\Organization;
 
 class HomeController extends PageController
 {
 
     public static function getHome()
     {
+        $organization = new Organization();
         $content =  View::render('home', [
-            "name" => "Estrutura MVC com php",
-            "description" =>  "Uma estrutura MVC com php puro do zero"
+            "id" => $organization->id,
+            "name" => $organization->name,
+            "description" =>  $organization->description
         ]);
 
         return parent::getPage("MVC com php", $content);
---
+++ app/Models/Entity/Organization.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Entity;
+
+class Organization
+{
+
+    public $id = 1;
+    public $name = "Estrutura MVC com php";
+    public $description = "Estrutura MVC com php puro do zero";
+}
